08edc7f11e12ca8183f0bc9ccc6827eb41220a9f
Muharrem <m.karapazar2@gmail.com>
2022-11-29 17:13:17 +0300

    Muharrem - HomePage Update

--- src/PageObject/Pages/HomePage.py
+++ src/PageObject/Pages/HomePage.py
@@ -15,6 +15,13 @@ class HomePage:
         self.accountVerify1_CSS = "h1[data-uia='stepTitle']"
         self.continueButton_CSS = "button[data-uia='cta-continue-registration']"
         self.accountVerify2_CSS = "h1[data-uia='stepTitle']"
+        self.oturumAcBtn_CSS = "a[data-uia='header-login-link']"
+        self.userLogin_ID = "id_userLoginId"
+        self.userPassword_ID = "id_password"
+        self.oturumAcBtn2_CSS = "button[data-uia='login-submit-button']"
+        self.loginNegativeVerifyMessage = "div[data-uia='text']"
+        self.loginEmptyVerifyMessage1 = "div[data-uia='login-field+error']"
+        self.loginEmptyVerifyMessage2 = "div[data-uia='password-field+error']"
 
 
     def get_acceptButton_CSS(self):
@@ -29,6 +36,20 @@ class HomePage:
         return self.driver.find_element(By.CSS_SELECTOR, self.accountVerify1_CSS)
     def get_continueButton_CSS(self):
         return self.driver.find_element(By.CSS_SELECTOR, self.continueButton_CSS)
+    def get_oturumAcBtn_CSS(self):
+        return self.driver.find_element(By.CSS_SELECTOR, self.oturumAcBtn_CSS)
+    def get_userLogin_ID(self):
+        return self.driver.find_element(By.ID, self.userLogin_ID)
+    def get_userPassword_ID(self):
+        return self.driver.find_element(By.ID, self.userPassword_ID)
+    def get_oturumAcBtn2_CSS(self):
+        return self.driver.find_element(By.CSS_SELECTOR, self.oturumAcBtn2_CSS)
+    def get_loginNegativeVerifyMessage(self):
+        return self.driver.find_element(By.CSS_SELECTOR, self.loginNegativeVerifyMessage)
+    def get_loginEmptyVerifyMessage1(self):
+        return self.driver.find_element(By.CSS_SELECTOR, self.loginEmptyVerifyMessage1)
+    def get_loginEmptyVerifyMessage2(self):
+        return self.driver.find_element(By.CSS_SELECTOR, self.loginEmptyVerifyMessage2)
 
     def scrollToElementMid(self, element):
         scrollElementIntoMiddle = "var viewPortHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);"
@@ -51,6 +72,8 @@ class HomePage:
             case "acceptButton_CSS": self.myElement = self.get_acceptButton_CSS()
             case "startButton_CSS": self.myElement = self.get_startButton_CSS()
             case "continueButton_CSS": self.myElement = self.get_continueButton_CSS()
+            case "oturumAcBtn_CSS": self.myElement = self.get_oturumAcBtn_CSS()
+            case "oturumAcBtn2_CSS": self.myElement = self.get_oturumAcBtn2_CSS()
         self.scrollToElementMid(self.myElement)
         EC.element_to_be_clickable(self.myElement)
         self.myElement.click()
@@ -58,6 +81,8 @@ class HomePage:
     def findAndSend(self, strElement, strText):
         match strElement:
             case "emailInput_ID": self.myElement = self.get_emailInput_ID()
+            case "userLogin_ID": self.myElement = self.get_userLogin_ID()
+            case "userPassword_ID": self.myElement = self.get_userPassword_ID()
         self.scrollToElementMid(self.myElement)
         EC.visibility_of(self.myElement)
         self.myElement.send_keys(strText)
@@ -66,6 +91,9 @@ class HomePage:
         match strElement:
             case "heroTitle_CSS": self.myElement = self.get_heroTitle_CSS()
             case "accountVerify1_CSS": self.myElement = self.get_accountVerify1_CSS()
+            case "loginNegativeVerifyMessage": self.myElement = self.get_loginNegativeVerifyMessage()
+            case "loginEmptyVerifyMessage1": self.myElement = self.get_loginEmptyVerifyMessage1()
+            case "loginEmptyVerifyMessage2": self.myElement = self.get_loginEmptyVerifyMessage2()
         self.scrollToElementMid(self.myElement)
         EC.visibility_of(self.myElement)
         assert textElement in self.myElement.text
